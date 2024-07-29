830e1ee4e42eedd06809ecf3b1cef2707d53b687
Zein Elabidine TRABELSI <zat@wevioo.com>
2024-07-29 08:48:38 +0100

    Setting Up LedgerEntrySetup table and service and jpa and api

---
+++ src/main/java/com/digithink/business_management/repository/GeneralLedgerSetupRepository.java
@@ -0,0 +1,9 @@
+package com.digithink.business_management.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.digithink.business_management.model.GeneralLedgerSetup;
+
+public interface GeneralLedgerSetupRepository extends JpaRepository<GeneralLedgerSetup, Long> {
+
+}
---
+++ src/main/java/com/digithink/business_management/service/GeneralLedgerSetupService.java
@@ -0,0 +1,21 @@
+package com.digithink.business_management.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import com.digithink.business_management.model.GeneralLedgerSetup;
+import com.digithink.business_management.repository.GeneralLedgerSetupRepository;
+
+@Service
+public class GeneralLedgerSetupService extends _BaseService<GeneralLedgerSetup, Long> {
+
+	@Autowired
+	private GeneralLedgerSetupRepository generalLedgerSetupRepository;
+
+	@Override
+	protected JpaRepository<GeneralLedgerSetup, Long> getRepository() {
+		return generalLedgerSetupRepository;
+	}
+
+}
