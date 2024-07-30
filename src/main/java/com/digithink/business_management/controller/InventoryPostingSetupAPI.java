9d1a9e2432fd10b642f289d208921e13a290369e
Zein Elabidine TRABELSI <zat@wevioo.com>
2024-07-30 08:43:05 +0100

    Setting up InventoryPostingSetup

---
+++ src/main/java/com/digithink/business_management/controller/InventoryPostingSetupAPI.java
@@ -0,0 +1,14 @@
+package com.digithink.business_management.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.digithink.business_management.model.InventoryPostingSetup;
+import com.digithink.business_management.service.InventoryPostingSetupService;
+
+@RestController
+@RequestMapping("inventory_posting_setup")
+public class InventoryPostingSetupAPI
+		extends _BaseController<InventoryPostingSetup, Long, InventoryPostingSetupService> {
+
+}
