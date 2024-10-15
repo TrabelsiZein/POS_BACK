536d008fff1115f0a281777a784954669037dd5e
Zein Elabidine TRABELSI <zat@wevioo.com>
2024-10-15 12:15:48 +0100

    return to the first project data structure

--- src/main/java/com/digithink/business_management/controller/ItemUnitOfMeasureAPI.java
+++ src/main/java/com/digithink/business_management/controller/ItemUnitOfMeasureAPI.java
@@ -1,9 +1,8 @@
-package com.digithink.business_management.controller.configuration;
+package com.digithink.business_management.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.digithink.business_management.controller._BaseController;
 import com.digithink.business_management.model.configuration.ItemUnitOfMeasure;
 import com.digithink.business_management.service.configuration.ItemUnitOfMeasureService;
 
